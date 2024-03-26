f0dbc5138506ec1a2208abb089b90f4d53b192d6
cuneyt aykac <cuneytaykac2@gmail.com>
2024-03-26 09:54:41 +0300

    flavor_setup/ios schema editing

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -7,6 +7,8 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
+import 'package:flutter/widgets.dart';
+
 class $LibGen {
   const $LibGen();
 
@@ -21,6 +23,25 @@ class $AssetsColorGen {
 
   /// List of all assets
   List<String> get values => [colors];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/dev_logo.png
+  AssetGenImage get devLogo =>
+      const AssetGenImage('assets/images/dev_logo.png');
+
+  /// File path: assets/images/prod_logo.png
+  AssetGenImage get prodLogo =>
+      const AssetGenImage('assets/images/prod_logo.png');
+
+  /// File path: assets/images/staging_logo.png
+  AssetGenImage get stagingLogo =>
+      const AssetGenImage('assets/images/staging_logo.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [devLogo, prodLogo, stagingLogo];
 }
 
 class $AssetsLocalizableGen {
@@ -56,6 +77,81 @@ class Assets {
   Assets._();
 
   static const $AssetsColorGen color = $AssetsColorGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
   static const $AssetsLocalizableGen localizable = $AssetsLocalizableGen();
   static const $LibGen lib = $LibGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
 }
